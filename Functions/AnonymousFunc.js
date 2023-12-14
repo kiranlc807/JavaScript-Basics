00f221517389371b660deec51c6f38d1e73757c0
unknown <kiran807lc@gmail.com>
2023-12-14 10:22:29 +0530

    [kirana]Added:msg

---
+++ Functions/AnonymousFunc.js
@@ -0,0 +1,10 @@
+// Anonymous function is one of the function it delcared with a name 
+// Anonymous function will assigned to a veriable  
+
+let add = function(num1,num2)  //function without name and assined to veriable.  
+{
+    return num1+num2
+}
+
+let result= add(20,20);       //calling the fuction useing the veriable name.
+console.log(result)
---
+++ Functions/RegularFunc.js
@@ -0,0 +1,13 @@
+//Function is a block of code desgined to perform a perticular task.
+
+
+function greet(u)          //function keyword with function name 
+{
+    return `Hello ${u}!!`  //return statement
+}
+
+let a = "Kirana"  
+
+let result =greet(a)       //function return value storing in a veriable  
+
+console.log(result)        // printing the result 

4d0f14bfd051116025b7d3205e62cb2c58ac8e1f
unknown <kiran807lc@gmail.com>
2023-12-14 10:17:09 +0530

    [kirana]Added:msg

---
+++ DataTypes/Data_Types.js
@@ -0,0 +1,51 @@
+// 1. Number
+let n1 = 10
+var n2 = 10
+let sum = n1+n2
+console.log(typeof sum)
+console.log(sum)
+
+
+console.log("--------------")
+
+// 2. String 
+let st1 = "Java"
+let st2 = "Script"
+var res = st1+st2
+console.log(typeof res)
+console.log(res)
+
+console.log("--------------")
+
+// 3. BigInt
+let num1 = 1234567890234567n
+let num2 = 9876543212345674n
+var res = num1+num2
+console.log(typeof res)
+console.log(res)
+
+console.log("--------------")
+
+// 4. Boolean
+let flag = true
+console.log(typeof flag)
+console.log(flag)
+
+console.log("--------------")
+
+// 5. null
+let nm = null
+console.log(typeof nm)
+console.log(nm)
+
+console.log("--------------")
+
+// 6. undefined
+let x
+console.log(typeof x)
+console.log(x)
+
+// 7. Symbol
+const sym = Symbol("Hi")
+console.log(typeof sym)
+console.log(sym)

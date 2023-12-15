59f26a02d1c6428f6c841c357d6413cf990ba48a
unknown <kiran807lc@gmail.com>
2023-12-15 16:26:17 +0530

    [kirana]Added:msg

---
+++ IIFE/prog2.js
@@ -0,0 +1,13 @@
+
+(function(dt) { 
+    console.log(dt.toLocaleTimeString()); // using refarance calling method
+    // Passing the Parameter. 
+})(new Date());
+
+
+(function(x){
+    for(let i=1;i<=x;i++){
+        console.log(i);
+    }
+})(10);
+
